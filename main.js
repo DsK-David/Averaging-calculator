f0646b1581ae680b1cddad098cc1fcc7f1438730
David silva <89320886+DsK-David@users.noreply.github.com>
2022-06-10 15:11:15 -0100

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 function calcular(){
     var fnote = document.querySelector(".fnote").value;
+    var snote = document.querySelector("#snote").value;
     var lnote = document.querySelector(".lnote").value;
    var resultado=document.getElementById("resultado")
    var resultado2=document.getElementById("resultado2")
-    var resultado1 = (parseInt(fnote) + parseInt(lnote))  / 2;
-   resultado.textContent="Sua media foi de "+resultado1
-   if(resultado1 < 10){
+    var calculo = (parseInt(fnote) + parseInt(snote) + parseInt(lnote))  / 2;
+    var resultadofinal = calculo.value
+   resultado.textContent="Sua media foi de "+resultadofinal
+   if(resultadofinal < 10){
        resultado2.textContent="Cuidado!!, com essa media cores o risco de reprovar de ano"
    }
 }
